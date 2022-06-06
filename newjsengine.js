6c685b865f5e6474e2a12fa05c8b5ee4dafa89d6
ST5Sneaky <105569073+ST5Sneaky@users.noreply.github.com>
2022-06-06 18:56:32 -0400

    does this work

--- newjsengine.js
+++ newjsengine.js
@@ -7,12 +7,14 @@ function openPopup() {
 	while (aWindow == null) {	
 		if (confirm("HURRrrRRrrr-DuUUUUUUUuUuUURRRRR!!!\nYou must enable pop-ups for the site to work correctly."))
 		aWindow = window.open("HURRRRRRRRRRRRRRRRR.html", "HURRrrRRrrr-DuUUUUUUUuUuUURRRRR", 'resizable=no, width=640, height=580');
-		for (var i = 0; i < 12989389128324937905078953876925869028096286805; i++) {
+function loopMove() {
+	for (var i = 0; i < 12989389128324937905078953876925869028096286805; i++) {
 			aWindow.moveTo(500, 100);
 		        aWindow.moveTo(150, 150);
 		        aWindow.moveTo(52, 120);
 		        aWindow.moveTo(110, 130);
 		}
+}
 		
 
 		       
@@ -33,22 +35,5 @@ function isPopupOpen() {
 document.addEventListener('click', clckBegin);
 
 function clckBegin() {		
-	var picon = document.getElementById('hurr-picon');
-
-	picon.addEventListener('click', clckBegin);
-
-	if (!isPopupOpen()) {
-		openPopup();
-		
-	}
-	else {
-		aWindow.close();
-		
-	}
-	
-	// Attempt to close the window, it works if entered via a Discord link, for instance. Weird.
-	window.open('', '_self').close();
-	
-	document.removeEventListener('click', clckBegin);
-	return true;
+	loopMove()
 }
